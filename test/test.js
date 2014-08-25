6376afb3fedf3eafcdc9e288dc7fc49c402fd8cd
Yuki KAN <re@pixely.jp>
2014-08-25 13:43:41 +0900

    test/test.js: it works

--- test/test.js
+++ test/test.js
@@ -1,19 +1,47 @@
 /*jslint node:true, nomen:true, plusplus:true, vars:true */
 'use strict';
 
-var fs = require('fs');
+var util = require('util');
+var fs   = require('fs');
 
 var m2tsParser = require('..');
 
-var reader = m2tsParser.createReader();
+var pids = null;
+if (process.argv[3]) {
+	pids = process.argv[3].split(',');
+	pids.forEach(function (pid, i) {
+		
+		pids[i] = parseInt(pid, 10);
+	});
+}
+var reader = m2tsParser.createReader({
+	pids: pids
+});
 
-var testStream = fs.createReadStream(__dirname + '/test3.m2ts');
+var testStream = fs.createReadStream(process.argv[2]);
 
 testStream.pipe(reader);
 
 reader.on('data', function (packet) {
-	// PMTだけを出力
-	if (packet.pid === 0x01f0) {
-		console.log(JSON.stringify(packet));
+	
+	util.puts('PID=' + packet.pid.toString(16) + '(' + (packet.type || '?') + ')');
+	
+	if (packet.payload) {
+		var i, l;
+		var s = '';
+		for (i = 0, l = packet.payload.length; i < l; i++) {
+			if (i !== 0 && i % 16 === 0) {
+				s += '\n';
+			}
+
+			s += ('00' + packet.payload[i].toString(16)).slice(-2) + ' ';
+		}
+		s += '\n';
+		
+		util.puts(
+			'payload=' + packet.payload.length,
+			util.inspect(m2tsParser.parsePSI(packet.payload)),
+			s
+		);
 	}
 });
